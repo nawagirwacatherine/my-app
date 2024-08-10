cb2e7a925038286b55d186fab5ce663dcadf379d
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2024-08-10 09:48:09 +0200

    added links to nav bar

--- src/App.js
+++ src/App.js
@@ -4,18 +4,32 @@ import Orderonline from './Orderonline.js';
 import Menu from './Menu';
 import Login from './Login';
 import Home from './Home';
-import {Routes,Route} from 'react-router-dom';
+import {Routes, Route, Link} from 'react-router-dom';
 
 
 
 
-function Header() {
-  return <h1>Little lemom restaurant</h1>;
-}
 
 function App() {
-  return <Header />;
+  return(
 
+    <div className="App">
+      <nav className="nav">
+        <Link to="/" className="nav-item">Home</Link>
+        <Link to="/Menu" className="nav-item">Menu</Link>
+        <Link to="/Orderonline" className="nav-item">Orderonline</Link>
+        <Link to="/Login" className="nav-item">Login</Link>
+
+      </nav>
+      <Routes>
+    <Route path="/" element={<Home />}/>
+    <Route path= "/Menu" element={<Menu />}/>
+    <Route path= "/Orderonline" element={<Orderonline />}/>
+    <Route path= "/Login" element={<Login />}/>
+    </Routes>
+    </div>
+
+
+  );
 }
-
 export default App;
