0ca920fed61e67791fe3b0f18026bc39d920f6df
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2024-10-23 08:09:09 +0200

    Added bacground photo and linear-gradient to login navigation

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,23 @@
 import './App.js';
+import image3Images from './images/image3.jpg';
 
 function Login() {
+    const backgroundStyle={
+
+         backgroundImage:`linear-gradient(rgba(0,0,0,0.2),#009688),url(${image3Images})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '100vh',
+    borderRadius: '10px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    
+    }
+        return(
+            <div style={backgroundStyle}></div>
+        )
+    
 
 }
 
