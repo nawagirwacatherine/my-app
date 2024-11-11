7beb3d7ce437bf139dfb74a8076034aaaacdfe5f
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2024-11-11 06:33:15 +0200

    Changed the images in about-us component

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -1,7 +1,7 @@
 import "./style.css"
-import imagesHighlight from './images/highlight.jpeg';
-import imagesHighlight1 from './images/highlight1.png';
-import imagesHighlight2 from './images/highlight2.jpg';
+import imagesMenu from './images/Menu.jpg';
+import imagesMenu1 from './images/Menu1.jpg';
+import imagesMenu2 from './images/Menu2.jpg';
 import Footer from './Footer';
 
 function AboutPage () {
@@ -11,10 +11,10 @@ function AboutPage () {
   <>
 
 <div className="Testimonials">
-  <h1>Testimonials</h1>
+  <h1>Read More About Us</h1>
   <div className="testimonial-section">
     <section className="testimonial-item">
-      <img src={imagesHighlight} alt="Testimonial 1" className="image" />
+      <img src={imagesMenu} alt="Testimonial 1" className="image" />
       <article>
       <p>
         "Little Lemon is a gem in Kampala! The food is bursting with authentic Mediterranean flavors, and the ambiance is so cozy and welcoming. It feels like a little escape to the Mediterranean!"
@@ -24,7 +24,7 @@ function AboutPage () {
     </section>
 
     <section className="testimonial-item">
-      <img src={imagesHighlight1} alt="Testimonial 2" className="image" />
+      <img src={imagesMenu1} alt="Testimonial 2" className="image" />
       <article>
       <p>
         "Perfect place for a family meal or a romantic dinner. The dishes are beautifully presented, and the staff treats you like family. My favorite spot in the city!"
@@ -34,7 +34,7 @@ function AboutPage () {
     </section>
 
     <section className="testimonial-item">
-      <img src={imagesHighlight2} alt="Testimonial 3" className="image" />
+      <img src={imagesMenu2} alt="Testimonial 3" className="image" />
       <article>
       <p>
         "The freshness of the ingredients really sets Little Lemon apart. Every bite is delicious, from the appetizers to the desserts. Highly recommend the grilled lamb and handmade pasta!"
